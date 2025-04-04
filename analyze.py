3947c79ef76ed0d962374cf7c7e199e111773e2b
kwrona05 <kwrona05@gmail.com>
2025-04-04 12:04:03 +0200

    count total number of countries and start planning my analyze

--- analyze.py
+++ analyze.py
@@ -4,3 +4,15 @@ from matplotlib import pyplot
 import seaborn as sns
 
 df = pd.read_csv('./Global_AI_Content_Impact_Dataset.csv')
+
+#Number of countries from which data was collected
+countries_number = df['Country'].count()
+print(f'Number of countries: {countries_number}')
+
+#Posegregować na lata i wyciągnąć dane:
+#Industries which used AI
+# AVG global AI adoption rate in each industry in each year
+
+
+
+
