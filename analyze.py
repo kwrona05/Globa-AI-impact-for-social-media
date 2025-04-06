b9752d70db39d6572ab7887c1df7d590df9761c6
kwrona05 <kwrona05@gmail.com>
2025-04-06 14:24:39 +0200

    analyzing and comparing next columns

--- analyze.py
+++ analyze.py
@@ -7,32 +7,32 @@ df = pd.read_csv('./Global_AI_Content_Impact_Dataset.csv')
 columns = df.columns.values
 print(columns)
 
-india_df = df[df['Country'] == 'India']
-france_df = df[df['Country'] == 'France']
-japan_df = df[df['Country'] == 'Japan']
-china_df = df[df['Country'] == 'China']
-usa_df = df[df['Country'] == 'USA']
-skorea_df = df[df['Country'] == 'South Korea']
-uk_df = df[df['Country'] == 'UK']
-germany_df = df[df['Country'] == 'Germany']
-canada_df = df[df['Country'] == 'Canada']
-australia = df[df['Country'] == 'Australia']
-
+grouped_countries = df.groupby('Country')
 #Market share of AI in companies
 ai_in_companies = df['Market Share of AI Companies (%)']
 avg_market_share = np.average(ai_in_companies)
 print(avg_market_share)
 
-india_avg_market_share = np.average(india_df['Market Share of AI Companies (%)'])
-print(india_avg_market_share)
-
+market_share_by_countries = df.groupby('Country')['Market Share of AI Companies (%)'].mean()
+for country, avg in market_share_by_countries.items():
+    print(f'{country}: {avg}')
 #Consumer trust to AI
+avg_consumer_trust_to_ai = np.average(df['Consumer Trust in AI (%)'])
+print(f'Global Consumer Trust: {avg_consumer_trust_to_ai}')
 
+consumer_trust_to_ai_by_countries = df.groupby('Country')['Consumer Trust in AI (%)'].mean()
+for country, avg in consumer_trust_to_ai_by_countries.items():
+    print(f'{country}: {avg}')
 #AI generated content
+avg_ai_generated_content = np.average(df['AI-Generated Content Volume (TBs per year)'])
+print(f'Global avg AI generated content: {avg_ai_generated_content}')
 
+generated_content_by_countries = df.groupby('Country')['AI-Generated Content Volume (TBs per year)'].mean()
+for country, avg in generated_content_by_countries.items():
+    print(f'{country}: {avg}')
 #Top AI tools
-
-
+most_popular_ai_tools = df['Top AI Tools Used'].value_counts()
+print(most_popular_ai_tools)
 
 #Number of countries from which data was collected
 countries_number = df.value_counts('Country')
